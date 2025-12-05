aadff7c0734c3d8bcab746f5ac8481db46e5b18e
buv <buv@gmx.de>
2025-12-05 01:32:26 +0100

    base for use of ssd1306 display

--- files/dfld_box/dfld/__init__.py
+++ files/dfld_box/dfld/__init__.py
@@ -1,5 +1,5 @@
-from .DataSink import DataSink, MqttDataSink
-from .DataSource import DataSource, AkModulDataSource, Bme280DataSource, DNMSDataSource, DNMSi2cDataSource, UdpDataSource
+from .DataSink import DataSink, MqttDataSink, SSD1306DataSink
+from .DataSource import DataSource, AkModulDataSource, Bme280DataSource, DNMSDataSource, DNMSi2cDataSource, UdpDataSource, MqttDataSource
 from .EventLoop import EventLoop
 from .LiveView import LiveView
 from .util import calc_crc, obfuscate_string, deobfuscate_string
